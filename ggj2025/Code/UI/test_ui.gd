f6e86a897d318809da8177918d0d385716aecb87
Lenetis <70149832+Lenetis@users.noreply.github.com>
2025-01-25 00:46:52 +0100

    Removed debug prints
    oops

--- ggj2025/Code/UI/test_ui.gd
+++ ggj2025/Code/UI/test_ui.gd
@@ -18,14 +18,10 @@ func _spawn_structure_ghost() -> StructureGhost:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_ESCAPE):
-		print("esc")
 		if _structure_placer.has_structure_ghost():
-			print("cancel structure")
 			_structure_placer.clear_structure_ghost()
 	if Input.is_key_pressed(KEY_1):
-		print("1")
 		if not _structure_placer.has_structure_ghost():
-			print("set structure")
 			var ghost = _spawn_structure_ghost()
 			_structure_placer.set_structure_ghost(ghost)
 			
